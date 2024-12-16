daad3ec1f6d432d43cbbe22e58177f52f4058693
edenx909 <edenx909@gmail.com>
2024-12-16 15:46:17 +1100

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,16 @@
-import React, { useState, useEffect } from "react";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import InfoPage from "./components/InfoPage";
 
-const clientID = import.meta.env.VITE_CLIENT_ID;
-const accessToken = `Bearer ${import.meta.env.VITE_AUTHORIZATION}`;
-
-const Home = () => {
-  const [search, setSearch] = useState("elden ring");
-  const [data, setData] = useState([]);
-  const fetchData = async () => {
-    try {
-      const response = await fetch("/api/games", {
-        method: "POST",
-        headers: {
-          "Client-ID": clientID,
-          Authorization: accessToken,
-          "Content-Type": "text/plain",
-        },
-        body: `fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,collections,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites; search"=${search}";`,
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch data");
-      }
-
-      const jsonData = await response.json();
-      setData(jsonData);
-      console.log(jsonData);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
+const App = () => {
   return (
-    <div>
-      <input
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Search for a game..."
-      />
-      <button onClick={() => fetchData()}>Search</button>
-      <div>
-        {data.length > 0 ? (
-          data.map((game) => (
-            <div key={game.id}>
-              <h3>{game.name}</h3>
-              <p>{game.slug}</p>
-              <p>{game.storyline}</p>
-            </div>
-          ))
-        ) : (
-          <p>No games found</p>
-        )}
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/info/:id" element={<InfoPage />} />
+      </Routes>
+    </Router>
   );
 };
 
-export default Home;
+export default App;
---
+++ src/hooks/useFetchGames.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+const clientID = import.meta.env.VITE_CLIENT_ID;
+const accessToken = `Bearer ${import.meta.env.VITE_AUTHORIZATION}`;
+
+const useFetchGames = () => {
+  const [data, setData] = useState(null);
+  const fetchData = async (search) => {
+    try {
+      const response = await fetch("/api/games", {
+        method: "POST",
+        headers: {
+          "Client-ID": clientID,
+          Authorization: accessToken,
+          "Content-Type": "text/plain",
+        },
+        body: `fields aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,collections,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites; search"=${search}";`,
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const jsonData = await response.json();
+      setData(jsonData);
+      console.log(jsonData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return { data, fetchData };
+};
+
+export default useFetchGames;
