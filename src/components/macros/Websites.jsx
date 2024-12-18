9326710b9248deb96b67a951027e6518187a2d95
edenx909 <edenx909@gmail.com>
2024-12-18 18:05:55 +1100

    now assigned to icons

--- src/components/macros/Websites.jsx
+++ src/components/macros/Websites.jsx
@@ -1,15 +1,51 @@
+// svg icons
+import { Twitch } from "../../assets/icons/Twitch";
+import { Steam } from "../../assets/icons/Steam";
+import { Wikipedia } from "../../assets/icons/Wikipedia";
+import { Twitter } from "../../assets/icons/Twitter";
+
 const Websites = ({ websiteData }) => {
+  console.log(websiteData);
+  const assignedIcons = (url) => {
+    if (url.includes("twitch.tv")) {
+      return <Twitch />;
+    }
+    if (url.includes("wikipedia.org")) {
+      return <Wikipedia />;
+    }
+    if (url.includes("store.steampowered.com")) {
+      return <Steam />;
+    }
+    if (url.includes("twitter.com")) {
+      return <Twitter />;
+    }
+    return null;
+  };
+
+  const filteredData = websiteData?.filter(
+    (item) => assignedIcons(item.url) !== null
+  );
+
+  const GameLinks = ({ url }) => {
+    const Icon = assignedIcons(url);
+    return (
+      <div
+        onClick={() => window.open(url, "_blank")}
+        className="cursor-pointer "
+      >
+        {Icon}
+      </div>
+    );
+  };
   return (
-    <div>
-      <h1>Websites</h1>
-      {websiteData?.map((url) => (
-        <div key={url.id}>
-          <a key={url.id} href={url.url}>
-            {url.url}
-            <br />
-          </a>
-        </div>
-      ))}
+    <div className="flex items-center space-x-4  justify-center ">
+      {filteredData?.length > 0 ? (
+        filteredData.map((websites) => (
+          <GameLinks key={websites.id} url={websites.url} />
+        ))
+      ) : (
+        <>No Links</>
+      )}
     </div>
   );
 };
