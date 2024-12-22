dee052e2dadae05a0add1b710f7424cb597f76a7
edenx909 <edenx909@gmail.com>
2024-12-23 09:42:04 +1100

    more relevant results using ratings

--- src/hooks/useFetchGames.jsx
+++ src/hooks/useFetchGames.jsx
@@ -13,7 +13,7 @@ const useFetchGames = () => {
           Authorization: accessToken,
           "Content-Type": "text/plain",
         },
-        body: `fields aggregated_rating,artworks,cover,created_at,dlcs,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites; search"=${search}";`,
+        body: `fields aggregated_rating,artworks,cover,created_at,dlcs,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites; search"=${search}"; where total_rating_count > 0 ;`,
       });
 
       if (!response.ok) {
