173c613c730671bdb205938e2d988b5d0f4cb8f5
edenx909 <edenx909@gmail.com>
2024-12-25 16:09:08 +1100

    fix vercel

--- src/hooks/useFetchArtwork.jsx
+++ src/hooks/useFetchArtwork.jsx
@@ -1,12 +1,13 @@
 import { useState, useCallback } from "react";
 const clientID = import.meta.env.VITE_CLIENT_ID;
 const accessToken = `Bearer ${import.meta.env.VITE_AUTHORIZATION}`;
+const apiURL = import.meta.env.VITE_API_URL;
 
 const useFetchArtwork = () => {
   const [artworkData, setArtworkData] = useState(null);
   const fetchArtwork = useCallback(async (id) => {
     try {
-      const response = await fetch("/api/artworks", {
+      const response = await fetch(`/${apiURL}/artworks`, {
         method: "POST",
         headers: {
           "Client-ID": clientID,
@@ -16,7 +17,7 @@ const useFetchArtwork = () => {
         body: `fields image_id,url; where game=${id};`,
       });
       if (!response.ok) {
-        throw new Error("Failed to fetch Cover data");
+        throw new Error("Failed to fetch Artwork data");
       }
       const jsonData = await response.json();
       setArtworkData(jsonData);
